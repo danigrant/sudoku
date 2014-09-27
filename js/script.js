9bdf4652c30ed4587393546df04e26a819e41005
danigrant <thedanigrant@gmail.com>
2014-09-27 00:24:10 -0400

    auto-validate toggle && validate board

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//example board
 var example = {
 	initial: [
 		"---6--4--",
@@ -26,17 +27,20 @@ var example = {
 
 $(document).ready(function(){
 	var sudokuGame = new Sudoku(example.initial, example.solution);
-	load(sudokuGame);
-
 	var autoValidate = false;
 
+	load(sudokuGame);
+
 	$('#board').on('keydown', '.sudoku-box', function(e){
+		//only accepts inputs from backspace(8), tab(9), arrows(37-40), 0-9(48-57 && 96-105), enter(13), delete(46)
 		if([8,9,37,38,39,40,48,49,50,51,52,53,54,55,56,57,13,46,96,97,98,99,100,101,102,103,104,105].indexOf(e.which) === -1){
 			e.preventDefault();
 		} 
 		else {
-			if (autoValidate) validateBoard();
+			//if input from accepted key, on keydown - check if the key matches the solution
+			if (autoValidate) validateBox();
 		}
+		//on arrow keys, move around the board
 		switch(e.which){
 			case 37: // left arrow
 				var c = $(this);
@@ -84,43 +88,84 @@ $(document).ready(function(){
 				break;
 		}
 	});
+	//automatically selects the current square after input
 	$('#board').on('keyup', '.sudoku-box', function(e){
 		$(this).select();
 	});
+
 	$('#board').on('focus', '.sudoku-box', function(e){
 		$(this).select();
 	});
 	$('#board').on('mouseup', '.sudoku-box', function(e){
 		e.preventDefault();
 	});
+	
+	//when the validate toggle's value is changed, if it is now checked, validate the board
 	$('#toggle-validation').change(function() {
 		autoValidate = $(this).prop('checked');
-		if (autoValidate) validateBoard();
+		if (autoValidate) validateBox();
+		else {
+			$('.valid').removeClass('valid');
+			$('.invalid').removeClass('invalid');
+		};
 	});
-	function validateBoard() {
-		$('.valid').removeClass('valid');
-		$('.invalid').removeClass('invalid');
+
+	function validateBox() {
 		sudokuGame.board.forEach(function(currentRow, rowIndex) { // rowIndex is 0 indexed
 			currentRow.forEach(function(currentEl, colIndex) { // colIndex is 0 indexed
-				console.log(currentEl);
-
-				var currentBox = $('.sudoku-box[data-row="'+(rowIndex+1)+'"][data-col="'+(colIndex+1)+'"]');
+				//wrap the next logic in if the box is not a preset (i.e. can be validated)
+				var currentBox = getCurrentBox(rowIndex, colIndex);
+				currentBox.removeClass('valid');
+				currentBox.removeClass('invalid');
 				if(currentBox.val() === "") { 
+					//TODO replace empty string
 					//empty string
 				} 
 				else if(currentEl.solution === Number(currentBox.val())) {
 					currentBox.addClass('valid');
 				}
 				else {
-					console.log(currentBox.val());
-					console.log(currentBox.solution);
 					currentBox.addClass('invalid');
 				}
 			});
 		})
 	}
+
+	$('#submit').click(validateBoard);
+
+	function validateBoard() {
+		//check for each box that value matches solution
+		var validGame = true;
+		sudokuGame.board.forEach(function(currentRow, rowIndex) { // rowIndex is 0 indexed
+			currentRow.forEach(function(currentEl, colIndex) {
+				currentBox = getCurrentBox(rowIndex, colIndex);
+				
+				if(currentBox.prop('enabled')) {
+					console.log("this is the value : " + currentBox.val());
+					console.log("this is the solution : " + currentEl.solution);
+					if (currentBox.val() == currentEl.solution) {
+						//woot
+					}
+					else {
+						//this elem is false
+						currentBox.addClass('invalid');
+						console.log("returning false. invalid.")
+						validGame = false;
+					}
+				}
+			})
+		})
+		console.log(validGame);
+		return validGame;
+	}
+
+	//convert from obj model to the corresponding DOM elem
+	function getCurrentBox(rowIndex, colIndex) {
+		return currentBox = $('.sudoku-box[data-row="'+(rowIndex+1)+'"][data-col="'+(colIndex+1)+'"]:not(.sudoku-box:disabled)');
+	}
 });
 
+//Sudoku constructor takes the initial setting of the board && the board's solution 
 var Sudoku = (function(initial, solution){
 	
 	function Sudoku(initial, solution){
@@ -130,27 +175,30 @@ var Sudoku = (function(initial, solution){
 	}
 	
 	Sudoku.prototype.generateBoard = function() {
-		var tempRay = [];
+		var tempArray = [];
 		for(var i = 1; i < 10; i++){
 			var tempRow = [];
 			for(var j = 1; j < 10; j++){
 				var currentInitialEl = this.initial[i-1][j-1];
-				if(!validCharacter(currentInitialEl))
+				if(!validCharacter(currentInitialEl)) {
 					console.error('Input character at ('+i-1+','+j-1+') is not recognized. Accepted characters are {-,1,2,3,4,5,6,7,8,9}');
+					throw new Error("invalid characters");
+				}
 				tempRow.push({
 					value: currentInitialEl == "-" ? "" : currentInitialEl,
 					mutable: currentInitialEl == "-",
 					solution: Number(this.solution[i-1][j-1])
 				});
 			}
-			tempRay.push(tempRow);
+			tempArray.push(tempRow);
 		}
-		return tempRay;
+		return tempArray;
 	}
 	
 	return Sudoku;
 })();
 
+//a char is valid if it is '-' (indicates an empty square), an integer, and between 1 and 9 
 function validCharacter(c){
 	var cToNum = Number(c);
 	return c === "-" || ( cToNum % 1 === 0 && 1<= cToNum && cToNum <= 9)
@@ -160,11 +208,9 @@ function validCharacter(c){
 function load(sudokuGame) {
 	sudokuGame.board.forEach(function(currentRow, rowIndex) { // rowIndex is 0 indexed
 		currentRow.forEach(function(currentEl, colIndex) { // colIndex is 0 indexed
-			console.log(currentEl);
 			$('#board').append('<input class="sudoku-box" data-row="'+(rowIndex+1)+'" data-col="'+(colIndex+1)+'" type="text" min="0" max="9" maxlength="1" value="'+ currentEl["value"]+'" '+ (currentEl.mutable ? "": "disabled") +'>');
-			// Input type number doesn't support maxlength
+			// Input type 'number' doesn't support maxlength. Thus, input type 'text'
 		});
 		$('#board').append('<br />');
 	})
 }
-
