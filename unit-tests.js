2fb99fc9ed0fe901e0359563cd9cadadc5554c75
danigrant <thedanigrant@gmail.com>
2014-09-29 13:43:25 -0400

    mobile design

---
+++ unit-tests.js
@@ -0,0 +1,17 @@
+function testInitialInput() {
+	//if an initial board includes characters not considered valid, will the board return an error
+}
+
+function testNullBoard() {
+	//does the program catch a null board? if null initial.example, or null initial.solution?
+}
+
+function testIncompleteInitialBoard() {
+	//if not all the solutions are listed in the array - (or if there are invalid chars in solution) will the program catch it?
+}
+
+function catchBadInput() {
+	//on keydown, if not an approved key, the sudoku board value at that spot should not be the value of the keycode from keydown
+}
+
+function
