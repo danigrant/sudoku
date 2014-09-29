eb09be01c06e3d8874857295a58f672bd08b5275
danigrant <thedanigrant@gmail.com>
2014-09-29 02:38:09 -0400

    added animations on completion

--- js/explode.js
+++ js/explode.js
@@ -11,8 +11,28 @@ function explode() {
 	});
 
 	//bring in the hidden congratulatory elems
-	$('.congrats').fadeTo(3500, 1, function() {
+	$('.congrats').fadeTo(3000, 1, function() {
 		//click here to play again fades in
 		$('.back').fadeTo(1000, 1);
 	});
+
+	//listen for 'go back'
+	$('.back').click(function() {
+		//reverse the fade outs, bring everything back in
+
+		//fade out the congratulatory elems
+		$('.back').fadeTo(50, 0);
+		$('.congrats').fadeTo(50, 0);
+
+		//fade in the board
+		$('input').each(function() {
+			$(this).fadeTo(5000*Math.random(),1);
+		});
+
+		//fade in the buttons
+		$('.option1').each(function() {
+			$(this).fadeTo(2000*Math.random(),1);
+		});
+
+	})
 }
